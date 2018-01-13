27d55bb173c917d9020b737e69a784ac9cd1f3e1
Katerina Plushko <katplush@yahoo.com>
2018-01-12 22:25:20 -0800

    add code for questions 2-5

--- js/app.js
+++ js/app.js
@@ -28,58 +28,62 @@ if (answer === 'no' || answer === 'n') {
     //if it's not correct, explain correct answer
     alert('Nope! I love me some chocolate but cheese is my jam.');
 }
-/*
+
 //Question 2
-var answer = prompt('').toLowerCase();
+var q2 = 'Did I keep a secret pet chinchilla in my college dorm room?'
+var answer = prompt(q2).toLowerCase();
 
-console.log(answer);
+console.log("User guessed:" , answer, " to question: ", q2);
 
-if (answer === 'yes' || answer === 'y') {
+if (answer === 'no' || answer === 'n') {
     //if it's correct give them a point
     userPoints++; 
 } else {
     //if it's not correct, exlain correct answer
-    alert('');
+    alert('Nope! But I had a secret pet python!');
 }
 
 //Question 3
-var answer = prompt('').toLowerCase();
+var q3 = 'Do I prefer music to TV?'
+var answer = prompt(q3).toUpperCase();
 
-console.log(answer);
+console.log("User guessed:" , answer, " to question: ", q3);
 
-if (answer === 'yes' || answer === 'y') {
+if (answer === 'YES' || answer === 'Y') {
     //if it's correct give them a point
     userPoints++; 
 } else {
-    //if it's not correct, explain correct answer
-    alert('');
+    //if it's not correct, exlain correct answer
+    alert('No way silly! Music is my aeroplane.');
 }
 
 //Question 4
-var answer = prompt('').toLowerCase();
+var q4 = 'Did I grow up on the West Coast?'
+var answer = prompt(q4).toLowerCase();
 
-console.log(answer);
+console.log("User guessed:" , answer, " to question: ", q4);
 
-if (answer === 'yes' || answer === 'y') {
+if (answer === 'no' || answer === 'n') {
     //if it's correct give them a point
     userPoints++; 
 } else {
-    //if it's not correct, explain correct answer
-    alert('');
+    //if it's not correct, exlain correct answer
+    alert('Nope! I grew up on the East coast in Mechanicsville, VA!');
 }
 
 //Question 5
-var answer = prompt('').toLowerCase();
+var q5 = 'Do I prefer 4th of July to Halloween?'
+var answer = prompt(q5).toUpperCase();
 
-console.log(answer);
+console.log("User guessed:" , answer, " to question: ", q5);
 
-if (answer === 'yes' || answer === 'y') {
+if (answer === 'YES' || answer === 'Y') {
     //if it's correct give them a point
     userPoints++; 
 } else {
-    //if it's not correct, expain correct answer
-    alert('');
+    //if it's not correct, exlain correct answer
+    alert('Nope! 4th of July is definitely my favorite!');
 }
 
-alert('you have' + userPoints + ' points.');
-*/
+
+alert('you have ' + userPoints + ' points. Thanks for playing!');
